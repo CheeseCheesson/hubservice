db9e38e3064050d853a1cdf55da9b26af8ecc4ab
CheeseCheesson <kira7999@gmail.com>
2023-05-14 21:13:44 +0300

    add chakra

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,18 +2,19 @@ import { useRoutes } from "react-router-dom";
 import "./App.css";
 import Home from "./../Pages/Home";
 import Layout from "../Layout/Layout";
-import R7 from "../Pages/R7";
 
 function App() {
   const routes = useRoutes([
-    { path: "/", element: <Layout />, children: [
-      { path: "/", element: <Home /> },
-      { path: "/r7", element: <R7 /> },
-    ] },
+    {
+      path: "/", element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+      ]
+    },
   ]);
   return (
     <div>
-        {routes}
+      {routes}
     </div>
   );
 }
