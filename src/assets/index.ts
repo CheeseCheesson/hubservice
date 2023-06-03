774dad8105f4812e7f97570d704dc77217faa2b3
CheeseCheesson <kira7999@gmail.com>
2023-06-03 08:51:58 +0300

    v1-icons

---
+++ src/assets/index.ts
@@ -0,0 +1,32 @@
+import cartridgewithprinter from './icons/cartridgewithprinter.png'
+import computer from './icons/computer.png'
+import factory from './icons/factory.png'
+import laptop from './icons/laptop.png'
+import machine from './icons/machine.png'
+import microwave from './icons/microwave.png'
+import monic from './icons/monic.png'
+import phone from './icons/phone.png'
+import printer from './icons/printer.png'
+import tablet from './icons/tablet.png'
+import map from './map/map.png'
+
+export const ImageItemsSrc = {
+  cartridgewithprinter,
+  computer,
+  factory,
+  laptop,
+  machine,
+  microwave,
+  monic,
+  phone,
+  printer,
+  tablet
+}
+
+export const ImageFactory = {
+  factory
+}
+
+export const ImageMap = {
+  map
+}
